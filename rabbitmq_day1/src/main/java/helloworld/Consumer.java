ee04e797908261be6f72733c7e50e4ee249dced6
szq <1300853631@qq.com>
2021-09-09 00:04:17 +0800

    新增了调用rabbitmq

---
+++ rabbitmq_day1/src/main/java/helloworld/Consumer.java
@@ -0,0 +1,45 @@
+package helloworld;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        // 设置连接的主机
+        connectionFactory.setHost("192.168.10.27");
+        // 设置端口号
+        connectionFactory.setPort(5672);
+        // 设置连接那个虚拟主机
+        connectionFactory.setVirtualHost("/ems");
+        // 设置访问虚拟主机的用户名和密码
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("szq999yy.");
+
+        //获取连接对象
+        Connection connection = connectionFactory.newConnection();
+
+        // 获取连接中的通道
+        Channel channel = connection.createChannel();
+
+        // 消费者参数与生产者保持一致
+        channel.queueDeclare("hello",false, false,false,null);
+
+        //消费消息时的回调接口
+        channel.basicConsume("hello",true, new DefaultConsumer(channel){
+
+            // 消息队列中取出的消息
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                super.handleDelivery(consumerTag, envelope, properties, body);
+                System.out.println("new String(body):" + new String(body));
+            }
+        });
+        // 不建议关闭，consumer一直运行
+        // channel.close();
+        // connection.close();
+    }
+}
---
+++ rabbitmq_day1/src/main/java/helloworld/provider.java
@@ -0,0 +1,44 @@
+package helloworld;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class provider {
+    // 生产消息
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        // 设置连接的主机
+        connectionFactory.setHost("192.168.10.27");
+        // 设置端口号
+        connectionFactory.setPort(5672);
+        // 设置连接那个虚拟主机
+         connectionFactory.setVirtualHost("/ems");
+         // 设置访问虚拟主机的用户名和密码
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("szq999yy.");
+
+        //获取连接对象
+        Connection connection = connectionFactory.newConnection();
+
+        // 获取连接中的通道
+        Channel channel = connection.createChannel();
+
+        // 获取通道中的队列
+        // 参数1队列名，如果不存在则创建
+        // 2，持久化， 3.是否独占，当前队列只能被当前连接可用 4消费挖不出后是否删除队列 5 额外参数
+        channel.queueDeclare("hello",false, false,false,null);
+
+        // 发布消息
+        // 1交换机名称 2队列名称 3 额外参数 4 消息具体内容
+        channel.basicPublish("","hello",null,"rabbitmq hello".getBytes(StandardCharsets.UTF_8));
+
+        channel.close();
+        connection.close();
+    }
+}
